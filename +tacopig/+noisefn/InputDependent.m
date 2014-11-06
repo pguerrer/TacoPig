1d6ad8c797ac45a45d65979f205e529c0d3b52c8
Pablo Guerrero <pguerrer@ing.uchile.cl>
2014-11-06 17:09:24 -0300

    Input dependent covariance function

---
+++ +tacopig/+noisefn/InputDependent.m
@@ -0,0 +1,33 @@
+classdef InputDependent < tacopig.noisefn.NoiseFunc
+   
+	properties
+		nPar,
+		inputDependentFunc,
+		inputDependentGrad
+	end
+	
+	methods
+		function n_theta = npar(obj)
+            n_theta = obj.nPar;
+		end
+		function noise = eval(obj, X, GP)
+             par = tacopig.noisefn.NoiseFunc.getNoisePar(GP);
+             noise = diag(obj.inputDependentFunc(X,par));
+        end
+        
+        function [g] = gradient(obj, X, GP)
+             par = tacopig.noisefn.NoiseFunc.getNoisePar(GP);
+			 if (isempty(obj.inputDependentGrad))
+				 g=[];
+			 else
+				g = diag(obj.inputDependentGrad(X,par));
+			 end
+        end
+	end
+	
+    % Most noise functions will be static
+    methods(Static) 
+        
+    end
+    
+end
